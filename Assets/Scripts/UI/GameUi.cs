4a226b42ff216b0d24b20b67adf93454e3113f2f
Ivan Popenya <popenya4554@gmail.com>
2022-11-11 21:52:30 +0300

    full update game

---
+++ Assets/Scripts/UI/GameUi.cs
@@ -0,0 +1,13 @@
+﻿using Systems;
+using UnityEngine;
+
+namespace UI
+{
+    public class GameUi : MonoBehaviour
+    {
+        [SerializeField] private GameWindow _gameWindow;
+
+        public void Initialize(GameSystemHandlers systemHandlers) => 
+            _gameWindow.Initialize(systemHandlers);
+    }
+}
